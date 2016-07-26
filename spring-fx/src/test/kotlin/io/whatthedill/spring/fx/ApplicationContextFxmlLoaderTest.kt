e18f2b8601eccb12060c9b2e447f8cec1de8487e
Jacob Wu <jwu@digitalmeasures.com>
2016-07-26 15:24:46 -0500

    trying out travis=ci

---
+++ spring-fx/src/test/kotlin/io/whatthedill/spring/fx/ApplicationContextFxmlLoaderTest.kt
@@ -0,0 +1,14 @@
+package io.whatthedill.spring.fx
+
+import org.junit.Assert.*
+import org.junit.Test
+
+open class ApplicationContextFxmlLoaderTest {
+
+    @Test
+    fun placeholder() {
+        //just need a passing test
+        assertTrue(true)
+    }
+
+}
